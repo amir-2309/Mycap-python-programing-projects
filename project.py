1bd91fe7a77c80427348b7b70d87a6eb852c9a22
amir-2309 <148235264+amir-2309@users.noreply.github.com>
2023-10-27 20:53:53 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,11 @@
+1.r=float(input("Enter radius of circle:"))
+a=3.14159*r*r
+ print("Area of circle=,")
+
+OUTPUT
+Enter radius of circle:7.5
+Area of circle=176.7144375
+
+2.filename=input("Input the filename")
+f extins=filename split (".")
+ print("the extension is:'txt')
